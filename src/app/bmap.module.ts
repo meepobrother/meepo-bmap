88d9d9524052b61921b753c60d7f4c3281ecdb67
15138730370 <1037483576@qq.com>
2017-12-25 10:45:19 +0800

    save to back

--- src/app/bmap.module.ts
+++ src/app/bmap.module.ts
@@ -22,7 +22,7 @@ import { AxiosModule } from 'meepo-axios';
 @NgModule({
     imports: [
         CommonModule,
-        AxiosModule,
+        AxiosModule.forRoot(),
         MeepoCoreServiceModule
     ],
     exports: [
