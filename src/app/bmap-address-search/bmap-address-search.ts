e42394d5d8f5ce90a284ac36dd4b46a9e5440a6d
15138730370 <1037483576@qq.com>
2018-01-05 17:58:11 +0800

    save to back

--- src/app/bmap-address-search/bmap-address-search.ts
+++ src/app/bmap-address-search/bmap-address-search.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'bmap-address-search',
-    templateUrl: './bmap-address-search.html'
+    templateUrl: './bmap-address-search.html',
+    styleUrls: ['./bmap-address-search.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class BmapAddressSearchComponent implements OnInit {
     constructor() { }
 
     ngOnInit() { }
+
+    down(e: any) {
+        console.log('down');
+        e.next(false);
+    }
+
+    up(e: any) {
+        console.log('up');
+        e.next(false);
+    }
 }
